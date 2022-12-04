7a1f423361c5cca7287cf5d42fb22fd247f7fb4d
Tammy-Lee <tammy-lee.plamondon@telus.com>
2022-12-04 15:14:29 -0800

    I think I broke it?

--- play.js
+++ play.js
@@ -29,6 +29,16 @@ const connect = function () {
   return conn;
 };
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
 console.log("Connecting ...");
 connect();
 
