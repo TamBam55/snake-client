42294d81333961f2c795285005f1a8965e313aca
Tammy-Lee <tammy-lee.plamondon@telus.com>
2022-12-04 15:30:15 -0800

    adding handleUserInput for Ctrl-C

--- play.js
+++ play.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const { stdin } = require("process");
 
 // establishes a connection with the game server
 const connect = function () {
@@ -15,11 +16,11 @@ const connect = function () {
     conn.write("Name: TLP");
   });
 
-  // conn.on("connect", () => {
-  //   conn.write("Move: up");
-  //   conn.write("Move: right")
-  //   conn.write("Move: down")
-  // });
+  conn.on("connect", () => {
+    conn.write("Move: up");
+    conn.write("Move: right")
+    conn.write("Move: down")
+  });
 
 
   conn.on("data", (data) => {
@@ -36,7 +37,17 @@ const setupInput = function () {
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+  stdin.on("data", handleUserInput);
   return stdin;
+};
+
+const handleUserInput = function () {
+  // your code here
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
 };
 
 console.log("Connecting ...");
